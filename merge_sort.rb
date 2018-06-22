8acb2a635240117825ad2bcee1bd6a1bd4d1544c
Amjad Ali (MJ) <amjad.hamdi.ali@gmail.com>
2018-06-22 02:10:52 -0500

    Function logs index of middle element in array

--- merge_sort.rb
+++ merge_sort.rb
@@ -1,10 +1,10 @@
 p 'Merge Sort!'
 
 def merge_sort array
-	array.length
+	middle = array.length/2.to_i
 end
 
-arr = [1, 29, 67, 4, 3, 3, 0, 1, 9]
+arr = [1, 29, 67, 4, 3, 3, 0, 1, 9, 10]
 
-p merge_sort arr
+merge_sort arr
 
