63fa7a280e45a42054f7d87a2b6e55c185be8fb4
Amjad Ali (MJ) <amjad.hamdi.ali@gmail.com>
2018-06-22 03:23:18 -0500

    Function recursively divides array

--- merge_sort.rb
+++ merge_sort.rb
@@ -1,7 +1,10 @@
 p 'Merge Sort!'
 
+
+
 def merge_sort array
 	len = array.length
+
 	if len < 2
 		return array
 	end
@@ -12,11 +15,20 @@ def merge_sort array
 
 	right = array[middle..len]
 
-
+	merge(merge_sort(left), merge_sort(right))
+	
 
 end
 
-arr = [1]
+def merge left, right
+
+	return left, right
+	
+end
+
+
+
+arr = [1, 29, 67, 4, 7, 3, 0, 1, 9]
 
 p merge_sort arr
 
