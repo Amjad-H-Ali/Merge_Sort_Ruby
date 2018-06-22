c82faaa618d1866d3e8f77e3474bfed05e1b4d22
Amjad Ali (MJ) <amjad.hamdi.ali@gmail.com>
2018-06-22 02:17:21 -0500

    Function finds left side of array

--- merge_sort.rb
+++ merge_sort.rb
@@ -2,9 +2,13 @@ p 'Merge Sort!'
 
 def merge_sort array
 	middle = array.length/2.to_i
+
+	left = array[0..middle - 1]
+
+	p left
 end
 
-arr = [1, 29, 67, 4, 3, 3, 0, 1, 9, 10]
+arr = [1, 29, 67, 4, 7, 3, 0, 1, 9]
 
 merge_sort arr
 
