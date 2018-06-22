971bc81bf3c891e35238e26105a7d319ea25574a
Amjad Ali (MJ) <amjad.hamdi.ali@gmail.com>
2018-06-22 02:24:48 -0500

    Function store length of array in variable

--- merge_sort.rb
+++ merge_sort.rb
@@ -1,19 +1,20 @@
 p 'Merge Sort!'
 
 def merge_sort array
+	len = array.length
+	p len
 	middle = array.length/2.to_i
 
 	left = array[0..middle - 1]
 
 	right = array[middle..array.length]
 
-	p left
-
+	p left 
 	p right
 
 end
 
-arr = [1, 29, 67, 4, 7, 3, 0, 1, 9, 10]
+arr = [1, 29, 67, 4, 7, 3, 0, 1, 9]
 
 merge_sort arr
 
