c5af7f07aa0fd8a586aae78ce71fe5712162aacc
Amjad Ali (MJ) <amjad.hamdi.ali@gmail.com>
2018-07-07 11:37:25 -0500

    Merge function uses recursion to merge sub arrays in a sorted manner

--- merge_sort.rb
+++ merge_sort.rb
@@ -22,8 +22,16 @@ end
 
 def merge left, right
 
-	return left, right
-	
+	if left.empty?
+		right
+	elsif right.empty?
+		left
+	elsif left.first < right.first
+		[left.first] + merge(left[1..left.length], right)
+	else
+		[right.first] + merge(left, right[1..right.length])
+	end
+					
 end
 
 
